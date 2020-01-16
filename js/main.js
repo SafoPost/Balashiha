ca0c1114fa8d2e1405bd444eef6ec9c3ef8c8ec7
Safo <s.i.post@mai.ru>
2020-01-16 05:15:31 +0500

    feat: photo-section & swiper-slider

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  //initialize swiper when document ready
+  var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    initialSlide: 2,
+    autoHeight: true
+  })
+});
