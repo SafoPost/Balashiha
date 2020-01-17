1321d2edf8c5153454a5bf8136454f318ef3aa45
Safo <s.i.post@mai.ru>
2020-01-17 05:30:36 +0500

    feat: open/close modal

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
 $(document).ready(function () {
+
+  var modal = $('.modal');
+  var modalBtn = $('[data-toggle=modal]');
+  var closeBtn = $('.modal__close');
+  var send = $('.send');
+  var sendBtn = $('[data-toggle=modal-send]');
+  var closeSendBtn = $('.send__close');
+
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+  closeSendBtn.on('click', function () {
+    send.toggleClass('send--visible');
+  });
+
+  document.on('click', function (event) {
+    if (event.target == modal) {
+      modal.classList.remove('modal--visible');
+    }
+  });
+
+  document.on('keydown', function (event) {
+    if (event.keyCode === 27) {
+      modal.classList.remove('modal--visible');
+    }
+  });
+
   //initialize swiper when document ready
   var mySwiper = new Swiper('.swiper-container', {
     loop: true,
